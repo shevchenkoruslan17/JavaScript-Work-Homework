99dbf71395f8b9f84e989e63da2be5203203d285
shevchenkoruslan17 <shevchenko.ruslan10@gmail.com>
2026-03-17 21:47:40 +0200

    Виконую лекції 3 уроку

---
+++ Work/lesson3/lesson3-1/script.js
@@ -0,0 +1,27 @@
+let x = 0;
+x = x + 1; // x = 1
+console.log(x);
+
+x++;
+console.log(x); // x = 2
+
+x += 1;
+console.log(x);// x = 3
+
+x = x -1; //x = 2
+console.log(x);
+
+x *= 1; //1
+console.log(x);
+
+x--;
+console.log(x);// x = 0
+
+
+let x =0;
+++x;
+console.log(x);
+
+let a = 0;
+let b = a++;
+console.log(a,b); // a = 1, b = 0 
---
+++ Work/lesson3/lesson3-2/script.js
@@ -0,0 +1,14 @@
+let users =[
+    {name: "John", age: 31, status: false},
+    {name: "John", age: 32, status: true},
+    {name: "John", age: 33, status: true},
+    {name: "John", age: 33, status: false},
+    {name: "John", age: 45, status: true},
+    {name: "John", age: 23, status: false},
+    {name: "John", age: 35, status: true},
+    {name: "John", age: 37, status: false},
+];
+
+for (let user of users) {
+    console.log(user);
+}
