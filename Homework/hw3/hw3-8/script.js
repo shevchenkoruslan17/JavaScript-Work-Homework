9f0c65f15f101087b15bbae3d673a6387a3155f2
shevchenkoruslan17 <shevchenko.ruslan10@gmail.com>
2026-03-25 09:50:54 +0200

    Виконую dz  3 уроку

---
+++ Homework/hw3/hw3-8/script.js
@@ -0,0 +1,16 @@
+const users = [
+    {
+        id: 1,
+        name: 'John Smith',
+        username: 'John',
+        email: 'JonaSmith@gmail.com',
+        phone: '0123456789',
+        address: {
+            city: 'San Francisco',
+            street: 'San Francisco',
+            suite: 'Apt. 765',
+            zipCode: '0123456789',
+        }
+    }
+];
+const usersBox = document.querySelector('users-box');
