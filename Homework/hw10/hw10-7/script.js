627bfaadf95ce1c8fea4c6e780714770c12a5e70
shevchenkoruslan17 <shevchenko.ruslan10@gmail.com>
2026-05-23 18:31:46 +0300

    виконую ДЗ10

--- Homework/hw10/hw10-7/script.js
+++ Homework/hw10/hw10-7/script.js
@@ -4,3 +4,18 @@
 //
 // addToLocalStorage(arrayName:string,objToAdd:any{}):void
 
+function addToLocalStorage(arrayName, objToAdd) {
+    const lsItem = localStorage.getItem(arrayName);
+    if (!lsItem){
+        throw new Error('there is no such array');
+    }
+
+    const array = JSON.parse(lsItem);
+    if (typeof  objToAdd === 'object'){
+        array.push(objToAdd);
+    }
+
+    localStorage.setItem(arrayName, JSON.stringify(array));
+}
+
+addToLocalStorage('sessionsList',{});
---
+++ Homework/hw10/hw10-8/script.js
@@ -0,0 +1,27 @@
+// Створити 3 інпута та кнопку. Один визначає кількість рядків, другий – кількість ячеєк, третій – вміст ячеєк.
+//
+//     При натисканні кнопки вся ця інформація зчитується і формується табличка з відповідним вмістом.
+
+const table = document.getElementById('table');
+
+const tableGeneratorForm = document.form['tableGeneratorForm'];
+
+tableGeneratorForm.onsubmit = function (e) {
+    table.innerText = '';
+    e.preventDefault();
+    const linesValue = +tableGeneratorForm.lines.value;
+    const cellsValue = +tableGeneratorForm.cells.value;
+    const dataValue = tableGeneratorForm.data.value;
+    console.log(linesValue, cellsValue, dataValue);
+
+    for ( let i = 0; i < linesValue; i++){
+        const tr = document.createElement('tr');
+        for (let j = 0; j < cellsValue; j++){
+            const td = document.createElement('td');
+            td.innerText = dataValue;
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
+}
