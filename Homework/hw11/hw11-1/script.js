fe347dc9c08eb2d10466811c8bc728b290acc62e
shevchenkoruslan17 <shevchenko.ruslan10@gmail.com>
2026-06-02 12:24:06 +0300

    виконую  ДЗ11

---
+++ Homework/hw11/hw11-1/script.js
@@ -0,0 +1,46 @@
+// – взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+
+const cartsDiv = document.getElementById('carts')
+
+fetch('https://dummyjson.com/carts')
+        .then(value => value.json())
+        .then(cartsObject =>{
+            const {carts} = cartsObject;
+            console.log(carts);
+
+            for (const cart of carts) {
+                const div = document.createElement('div');
+                div.classList.add('cart-container');
+                const divWithInfo = document.createElement('div');
+                divWithInfo.innerText = `
+                "total": ${cart.total},
+                "discountedTotal": ${cart.discountedTotal},
+                "userId": ${cart.userId},
+                "totalProducts": ${cart.totalProducts},
+                "totalQuantity": ${cart.totalQuantity}
+                `;
+
+                const ol = document.createElement('ol');
+                for (const product of cart.products) {
+                    const li = document.createElement('li')
+                    const info = document.createElement('p')
+                    info.innerText = `
+                    "id": ${product.id},
+                    "title": ${product.title},
+                    "price": ${product.price},
+                    "quantity": ${product.quantity},
+                    "total": ${product.total},
+                    "discountPercentage": ${product.discountPercentage},
+                    "discountedTotal": ${product.discountedTotal},
+                    `;
+                    const img = document.createElement('img');
+                    img.src = product.thumbnail;
+                    li.append(img, info);
+                    ol.appendChild(li);
+                }
+
+
+                div.append(divWithInfo, ol);
+                cartsDiv.appendChild(div);
+            }
+        });
